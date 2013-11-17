f33b799c30506f412b109f8a3be5964a6c8beb49
tuukka <tuukka@localhost>
2013-11-17 20:34:48 +0200

    playing around with eclipses windowbuilder plugin

---
+++ src/com/tuukka/testigui/testi.java
@@ -0,0 +1,140 @@
+package com.tuukka.testigui;
+
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.TextArea;
+
+import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JTextArea;
+
+public class testi {
+
+	private JFrame frame;
+
+	private JTextArea textArea = new JTextArea();
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					testi window = new testi();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public testi() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("file");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmNew = new JMenuItem("new");
+		mnFile.add(mntmNew);
+		
+		JMenuItem mntmOpen = new JMenuItem("open");
+		mnFile.add(mntmOpen);
+		
+		JMenuItem mntmSave = new JMenuItem("save");
+		mnFile.add(mntmSave);
+		
+		JRadioButtonMenuItem rdbtnmntmSaveAs = new JRadioButtonMenuItem("save as");
+		mnFile.add(rdbtnmntmSaveAs);
+		
+		JMenuItem mntmQuit = new JMenuItem("quit");
+		mnFile.add(mntmQuit);
+		
+		JMenu mnEdit = new JMenu("edit");
+		menuBar.add(mnEdit);
+		
+		JMenuItem mntmLoadSpriteList = new JMenuItem("load sprite list");
+		mntmLoadSpriteList.addActionListener(new ActionListener() {
+			private Component panel;
+
+			public void actionPerformed(ActionEvent arg0) {
+
+                JFileChooser fileopen = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("xml files", "xml");
+                fileopen.addChoosableFileFilter(filter);
+
+                int ret = fileopen.showDialog(panel, "Open file");
+
+                if (ret == JFileChooser.APPROVE_OPTION) {
+                    File file = fileopen.getSelectedFile();
+                    String text = readFile(file);
+                    //parseDOM(file);
+                    textArea.setText(text);
+                }
+			}
+
+		});
+		
+		mnEdit.add(mntmLoadSpriteList);
+		
+		frame.getContentPane().add(textArea, BorderLayout.WEST);
+	}
+
+		
+	    public String readFile(File file) {
+
+        StringBuffer fileBuffer = null;
+        String line = null;
+
+        String fileString;
+		try {
+            FileReader in = new FileReader(file);
+            BufferedReader brd = new BufferedReader(in);
+            fileBuffer = new StringBuffer();
+
+            while ((line = brd.readLine()) != null) {
+                fileBuffer.append(line).append(
+                        System.getProperty("line.separator"));
+            }
+
+            in.close();
+            fileString = fileBuffer.toString();
+        } catch (IOException e) {
+            return null;
+        }
+        return fileString;
+    }	
+		
+}
